b635d4f86aa10ab60377bbbdac6b4d7fb2e10759
Rafael de Oliveira <raosilva01@gmail.com>
2021-03-03 10:54:35 -0300

    Error updated

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.main')
+@extends('adminlte::login')
 
-@section('content')
+<!-- @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -84,4 +84,4 @@
 @section('js')
     <script src="/js/scripts.js" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script
-@endsection
+@endsection -->
